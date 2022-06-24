09907afe0afda16e827b57dfb99d6eeb443fdb6d
yasin <yasinmohamad4831@gmail.com>
2022-06-24 20:22:25 +0430

    Register_user

--- Blog/account/urls.py
+++ Blog/account/urls.py
@@ -6,4 +6,5 @@ app_name = "account"
 urlpatterns = [
     path('', user_login, name='user_login'),
     path('logout', user_logout, name='user_logout'),
+    path('register',user_register, name='user_register'),
 ]
--- Blog/account/views.py
+++ Blog/account/views.py
@@ -20,3 +20,22 @@ def user_logout(request):
     logout(request)
     return redirect('/')
 
+
+def user_register(request):
+    if request.method == 'POST':
+        context = {'errors':[]}
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password != password2 :
+            context['errors'].append('Password is same')
+            return render(request, 'auth/register.html',context)
+        if User.objects.filter(username=username):
+            context['errors'].append('Username already exist')
+            return render(request, 'auth/register.html',context)
+
+        user = User.objects.create(username=username, email=email, password=password)
+        login(request,user)
+        return redirect('/')
+    return render(request, 'auth/register.html')
